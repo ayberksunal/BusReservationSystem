7337c1a54b743390629c2cdc2d884860c1d4d3f4
ayberksunal <ayberksunal@gmail.com>
2017-04-11 22:53:21 +0300

    BusReservationSystem
    Android Project for Bus Reservation System

---
+++ app/src/main/java/com/example/ayberk/busreservationsystem/BusPage.java
@@ -0,0 +1,148 @@
+package com.example.ayberk.busreservationsystem;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+public class BusPage extends Activity {
+
+    EditText date;
+    DatePickerDialog datePickerDialog;
+    Spinner origin,destination;
+    public static String selectedOrigin;
+    public static int mYear;
+    public static int mMonth;
+    public static int mDay;
+    public static int sYear;
+    public static int sMonth;
+    public static int sDay;
+    public static String selectedDestination;
+
+    List<String> originValues=Arrays.asList("Istanbul","Ankara", "Bolu", "Izmir", "Kocaeli", "Eskişehir", "Nevşehir", "Antalya","Muğla","Gaziantep","Çanakkale","Adana");
+    List<String> destinationValues=Arrays.asList("Istanbul","Ankara", "Bolu", "Izmir", "Kocaeli", "Eskişehir", "Nevşehir", "Antalya","Muğla","Gaziantep","Çanakkale","Adana");
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_page);
+        origin = (Spinner)findViewById(R.id.spOrigin);
+        destination=(Spinner)findViewById(R.id.spDest);
+        // initiate the date picker and a button
+        date = (EditText) findViewById(R.id.date);
+        // perform click event on edit text
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // calender class's instance and get current date , month and year from calender
+                final Calendar c = Calendar.getInstance();
+                mYear = c.get(Calendar.YEAR); // current year
+                mMonth = c.get(Calendar.MONTH); // current month
+                mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+                // date picker dialog
+                datePickerDialog = new DatePickerDialog(BusPage.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // set day of month , month and year value in the edit text
+                                date.setText(dayOfMonth + "/"
+                                        + (monthOfYear + 1) + "/" + year);
+                                sDay=dayOfMonth;
+                                sMonth=monthOfYear;
+                                sYear=year;
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+
+            }
+        });
+
+        final SpinnerAdapter adapter = new SpinnerAdapter(BusPage.this, android.R.layout.simple_list_item_1);
+        adapter.addAll(originValues);
+        origin.setAdapter(adapter);
+        origin.setSelection(adapter.getCount());
+
+        origin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+
+                selectedOrigin =origin.getSelectedItem().toString();
+
+                if(origin.getSelectedItem() == "Origin")
+                {
+
+                    //Do nothing.
+                }
+                else{
+
+                    //Toast.makeText(MainActivity.this, origin.getSelectedItem().toString(), Toast.LENGTH_LONG).show();
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+
+
+            }
+        });
+
+        final SpinnerAdapter adapter2 = new SpinnerAdapter(BusPage.this, android.R.layout.simple_list_item_1);
+        adapter2.addAll(destinationValues);
+        destination.setAdapter(adapter2);
+        destination.setSelection(adapter2.getCount());
+        destination.setPrompt("please select a destination");
+
+        destination.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                // TODO Auto-generated method stub
+
+               selectedDestination= destination.getSelectedItem().toString();
+                if(destination.getSelectedItem().toString().equals(selectedOrigin)){
+                    Toast.makeText(BusPage.this, "You can not choose the same city ", Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+
+
+
+            }
+        });
+
+
+
+    }
+    public  void searchBusButton(View v){
+
+        Intent intent = new Intent(this, ListTravel.class);
+        startActivity(intent);
+    }
+
+
+
+
+
+}
